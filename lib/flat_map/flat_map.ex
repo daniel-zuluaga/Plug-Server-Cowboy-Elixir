4f42f45a50b9b263f5c457f828a9c5d7b4e07129
daniel-zuluaga <daniel.zuluaga007@gmail.com>
2023-05-18 16:43:13 -0500

    [Feature/Add] This two file they are similar because the concat: Join two lists to return a new one and flat_map: helps us sort the list

---
+++ lib/flat_map/flat_map.ex
@@ -0,0 +1,6 @@
+defmodule FlatMap do
+  alias(Concat_List, as: Concat)
+  def flat_map(elements, func, acc \\ [])
+  def flat_map([], _func, acc), do: acc
+  def flat_map([h | t], func, acc), do: flat_map(t, func, Concat.concat(acc, func.(h)))
+end
